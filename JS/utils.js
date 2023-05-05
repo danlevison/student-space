5a4ee6a4d13d4276c74635a0007a12fc8628e167
dLevz <100078678+dLevz@users.noreply.github.com>
2023-05-05 14:30:45 +0100

    Added close on outside click for modals

--- JS/utils.js
+++ JS/utils.js
@@ -1,6 +1,20 @@
 import studentData from "./data.js"
 let studentDataFromLocalStorage = JSON.parse(localStorage.getItem("studentData")) || studentData
+const modal = document.querySelectorAll("[data-modal]")
 
+modal.forEach((modal) => {
+    modal.addEventListener("click", e => {
+        const dialogDimensions = modal.getBoundingClientRect()
+        if (
+          e.clientX < dialogDimensions.left ||
+          e.clientX > dialogDimensions.right ||
+          e.clientY < dialogDimensions.top ||
+          e.clientY > dialogDimensions.bottom
+        ) {
+          modal.close()
+        }
+      })
+})
 
 export { studentDataFromLocalStorage }
 
